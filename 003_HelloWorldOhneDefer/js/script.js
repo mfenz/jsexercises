93161205e193e88ce40b6ab92275d2fc02beac46
Marc <mf@thefuture.at>
2021-04-08 15:41:59 +0200

    003 HelloWorld ohne defer

---
+++ 003_HelloWorldOhneDefer/js/script.js
@@ -0,0 +1,4 @@
+// script.js
+// sucht das HTML-Element mit der ID "title" und ersetzt den Inhalt des 
+// HTML-Objektes mit "Hello World"
+document.getElementById("title").innerHTML = "Hello World";
