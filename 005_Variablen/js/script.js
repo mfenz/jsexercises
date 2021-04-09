044153250706c300b658fa122cb77fb6d0201f4c
Marc <mf@thefuture.at>
2021-04-09 19:02:51 +0200

    save

---
+++ 005_Variablen/js/script.js
@@ -0,0 +1,6 @@
+ // script.js
+let foo = 100; // foo ist jetzt eine number und die Zahl 100
+foo = "bar"; // foo ist jetzt ein string und "bar"
+foo = false; // foo ist jetzt ein boolean und false
+foo = true; // foo ist noch immer ein boolean und true
+console.log(foo); // foo auf der Konsole ausgeben (true)
