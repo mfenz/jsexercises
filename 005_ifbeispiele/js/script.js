89649567b3187e6e045988f42621eddabceedcfb
mf@thefuture.at <mf@thefuture.at>
2021-04-28 13:42:08 +0200

    if beispiele

---
+++ 005_ifbeispiele/js/script.js
@@ -0,0 +1,20 @@
+let alter = prompt('Wie alt sind Sie?', 40);
+
+let verheiratet = false;
+if(alter >= 70){ // Nur Personen >= 70 verheiratet!
+    verheiratet = true;
+}
+
+if(verheiratet == true){
+    console.log("Sie sind verheiratet!");
+} else {
+    console.log("Sie sind nicht verheiratet!");
+}
+
+if(alter >= 16 && alter < 18){
+    console.log("Bier und Wein");
+} else if(alter >= 18){
+    console.log("Darf alles trinken!!");
+} else {
+    console.log("Nein.");
+}
